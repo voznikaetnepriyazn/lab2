e3d6b3002dd59d8ae2e8aad153ee2805ea077fd5
Sveta <maslvadaria2004@yandex.ru>
2024-05-17 20:23:01 +0300

    метод с лямбдой через for, исправления в program

--- lab2/ContactList.cs
+++ lab2/ContactList.cs
@@ -16,12 +16,26 @@ namespace lab2
         public int CompareTo(object obj)
         {
             if (obj is Contact contacts) return this.CompareTo(contacts.surname);
-            else throw new ArgumentException();
+            throw new ArgumentException();
         }
         public void ByAlph()
         { 
-            this.Sort((f1,f2)=> f1.surname.CompareTo(f2.surname));
+            this.Sort((f1,f2)=> f1.surname.CompareTo(f2.surname)); //через for
         }
+        //{
+        //   for (int i = 0; i < this.Count; i++) пока i меньше общего числа элементов с списке
+        //   {
+        //       for (int j = i + 1; j < this.Count; j++) для сравнения последующего элемента списка с предыдущим
+        //       {
+        //           if (String.Compare(this[i].Fio(), this[j].Fio()) > 0) сравнение строки, полученной методом Fio с другой строкой. 1ая строка больше 2ой -- функция возвращает положительное число. По алфавиту 1ая строка следовательно идёт после 2ой
+        //           {
+        //               var abc = this[i];
+        //               this[i] = this[j]; текущий элемент списка заменяется следующим, т к последний должен стоять ранее, согласно порядку
+        //               this[j] = abc; предыдущий элемент теперь находится после того, который ранее следовал за ним 
+        //           } меняем местами элементы
+        //        }
+        //    }
+        //}
         public void GetContact()
         {
             foreach (var contact in this)
--- lab2/Program.cs
+++ lab2/Program.cs
@@ -2,13 +2,13 @@
 
 var contacts = new ContactList
 {
-    new Contact() { surname="Топский", name="Павел", thirdName="Сергеевич", telNum="85292973"};
-    new Contact() { surname = "Иванов", name = "Иван", thirdName = "Иванович", telNum = "7598264" };
-    new Contact() { surname = "Петров", name = "Пётр", thirdName = "Петрович", telNum = "58139987" };
-    new Contact() { surname = "Сидрова", name = "Коза", thirdName = "Ивановна", telNum = "335656444" };
-    new Contact() { surname = "Джонатан", name = "Линдоер", thirdName = "96", telNum = "62784729" };
+    new Contact() {surname="Топский", name="Павел", thirdName="Сергеевич", telNum="85292973" },
+    new Contact() { surname = "Иванов", name = "Иван", thirdName = "Иванович", telNum = "7598264" },
+    new Contact() { surname = "Петров", name = "Пётр", thirdName = "Петрович", telNum = "58139987" },
+    new Contact() { surname = "Сидрова", name = "Коза", thirdName = "Ивановна", telNum = "335656444" },
+    new Contact() { surname = "Джонатан", name = "Линдоер", thirdName = "96", telNum = "62784729" }
 
-    ContactList.ByAlph;
-    ContactList.GetContact;
 };
 
+contacts.ByAlph();
+contacts.GetContact();
